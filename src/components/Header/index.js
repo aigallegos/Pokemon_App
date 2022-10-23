fcf8678a6c0a4d49997de7382538390034d8ccbb
Abram Gallegos <abramig2@illinois.edu>
2022-10-23 15:37:49 -0500

    I think yes

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './styles.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        POKEDEX
+      </div>
+    );
+  }
+}
+
+export default Header;
